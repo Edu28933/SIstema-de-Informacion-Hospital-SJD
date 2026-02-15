b0553aaee42ca2f07a5cbbc93f8405e8c47d4ec4
Edu28933 <esolaresv@miumg.edu.gt>
2026-02-15 17:39:22 -0600

    Agregar archivos de proyecto.

---
+++ SIstema de Informacion Hospital SJD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIstema_de_Informacion_Hospital_SJD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
