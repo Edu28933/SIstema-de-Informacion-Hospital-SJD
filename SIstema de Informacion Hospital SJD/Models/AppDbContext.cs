7730eab7abade97e55026f161ca95e64b69ddaa1
Edu28933 <esolaresv@miumg.edu.gt>
2026-03-22 16:54:02 -0600

    nada

---
+++ SIstema de Informacion Hospital SJD/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SIstema_de_Informacion_Hospital_SJD.Models
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        { 
+
+        }
+    }
+}
