2d8a7edf786a91f6ec7a855457ac4835a4129d7c
Edu28933 <esolaresv@miumg.edu.gt>
2026-04-12 17:00:20 -0600

    app.run

--- SIstema de Informacion Hospital SJD/Program.cs
+++ SIstema de Informacion Hospital SJD/Program.cs
@@ -1,13 +1,13 @@
-using SIstema_de_Informacion_Hospital_SJD;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SIstema_de_Informacion_Hospital_SJD;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// desde aqu�
+//desde aqui
 var connectionString =
     builder.Configuration.GetConnectionString
     ("DefaultConnection");
@@ -16,19 +16,33 @@ builder.Services.AddDbContext<AppDbContext>(
     options => options.UseSqlServer
     (connectionString), ServiceLifetime.Transient);
 
-builder.Services.AddDefaultIdentity<IdentityUser>(
-    options =>
+builder.Services.AddDefaultIdentity<IdentityUser>
+    (options =>
     options.SignIn.RequireConfirmedAccount =
     false).AddEntityFrameworkStores<AppDbContext>();
-//hasta aqu�
 
+//Hasta aqui
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may
-    // want to change this for production
-    // escenarios con https./2d2 mc/acentos-
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
