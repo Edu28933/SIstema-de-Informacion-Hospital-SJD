c7ed7b4090f2d0c60a2adf14a77d0b892f0541fd
Astrid Chacon <achaconr1@miumg.edu.gt>
2026-05-03 16:44:02 -0600

    Cambios3

--- SIstema de Informacion Hospital SJD/Program.cs
+++ SIstema de Informacion Hospital SJD/Program.cs
@@ -1,13 +1,13 @@
-using SIstema_de_Informacion_Hospital_SJD;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using SIstema_de_Informacion_Hospital_SJD;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// desde aqu�
+//desde aqui
 var connectionString =
     builder.Configuration.GetConnectionString
     ("DefaultConnection");
@@ -16,21 +16,36 @@ builder.Services.AddDbContext<AppDbContext>(
     options => options.UseSqlServer
     (connectionString), ServiceLifetime.Transient);
 
-builder.Services.AddDefaultIdentity<IdentityUser>(
-    options =>
+builder.Services.AddDefaultIdentity<IdentityUser>
+    (options =>
     options.SignIn.RequireConfirmedAccount =
     false).AddEntityFrameworkStores<AppDbContext>();
-//hasta aqu�
 
+//Hasta aqui
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
     // The default HSTS value is 30 days. You may
     // want to change this for production
     // escenarios con https./2d2 mc/acentos-
+=======
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+>>>>>>> 2d8a7edf786a91f6ec7a855457ac4835a4129d7c
+=======
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+>>>>>>> 2d8a7edf786a91f6ec7a855457ac4835a4129d7c
+=======
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+>>>>>>> 2d8a7edf786a91f6ec7a855457ac4835a4129d7c
 }
 
 app.UseHttpsRedirection();
@@ -38,6 +53,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 app.UseAuthentication(); // agregar esta linea
 app.UseAuthorization();
 
@@ -54,3 +72,24 @@ app.MapRazorPages();
 
 app.Run();
 
+=======
+=======
+>>>>>>> 2d8a7edf786a91f6ec7a855457ac4835a4129d7c
+=======
+>>>>>>> 2d8a7edf786a91f6ec7a855457ac4835a4129d7c
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 2d8a7edf786a91f6ec7a855457ac4835a4129d7c
+=======
+>>>>>>> 2d8a7edf786a91f6ec7a855457ac4835a4129d7c
+=======
+>>>>>>> 2d8a7edf786a91f6ec7a855457ac4835a4129d7c
